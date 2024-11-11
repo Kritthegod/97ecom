1dcd4f672170cafcf006732f4548eaa01623661a
Kritthegod <142289388+Kritthegod@users.noreply.github.com>
2024-11-11 11:46:24 +0630

    first connection

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$conn = new PDO("mysql:host=$host;port=3306;dbname=test97",
+                $user,$password);
+                echo "connection got";
+           
